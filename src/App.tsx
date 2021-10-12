7086222d601d600c7ee2855a36a8858f85f54e16
Keita Nagara <nagara.keita@gmail.com>
2021-10-12 10:50:05 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Practice1 } from "./practicies/Practice1";
+import { Practice2 } from "./practicies/Practice2";
+import { Practice3 } from "./practicies/Practice3";
+import { Practice4 } from "./practicies/Practice4";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Practice1 />
+      <Practice2 />
+      <Practice3 />
+      <Practice4 />
+    </div>
+  );
+}
